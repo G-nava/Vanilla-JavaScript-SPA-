1bb38cae083e1a95f7b4d3be44de3ab1dad33c2f
G-nava <navarro9207@gmail.com>
2022-04-09 22:55:52 -0500

    deining better routes

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -1,7 +1,7 @@
 const Header = () =>{
     const view = `
-        <div class="header">
-            <a class="header__items">Home</a>
+        <div class="header__container">
+            <a class="header__items">Guajira Visuals</a>
             <div class="header__items">
                 <a class="items" href="/">About me</a>
                 <a class="items" href="#/stack">My stack</a>
--- src/utils/resolveRoutes.js
+++ src/utils/resolveRoutes.js
@@ -1,9 +1,11 @@
 const resolveRoutes =(route)=>{
     if (route.length <=3){
         let validRoute = route === '/' ? route : '/:id';
+        // let validRoute = route';
         return validRoute
     }
     return `/${route}`;
+    
 }
 
 export default resolveRoutes
